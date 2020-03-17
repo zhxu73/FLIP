4a169e5cf568e5f53b34d3c0e96c8290602dfd90
John Xu <zhxu73@email.arizona.edu>
2020-03-17 01:24:48 -0700

    img_segmentation Dockerfile

---
+++ img_segmentation/entrypoint.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""
+Entry point for segmenting image
+"""
+
+import image_segmentation
+import argparse
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--directory', required=True, help="directory to ps2 collection")
+    parser.add_argument('-o', '--output', required=True, help="directory for output files.")
+    parser.add_argument('-p', '--processes', help="max spawnable processes used by multiprocessing", default=-1)
+
+    args = parser.parse_args()
+    return args
+
+def main():
+    args = parse_args()
+
+    # segmenting images using new python macro
+    print("segmenting images")
+    image_segmentation.process_collection(args.directory)
+
+if __name__ == "__main__":
+    main()
