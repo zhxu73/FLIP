0e6e4753f5af092089dd6d43d21a0571709c9975
John Xu <zhxu73@email.arizona.edu>
2020-03-27 12:21:24 -0700

    remove -o in entrypoint for bin2png and make use of -p

--- bin2png/entrypoint.py
+++ bin2png/entrypoint.py
@@ -10,9 +10,8 @@ import argparse
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-d', '--directory', required=True, help="directory to ps2 collection")
-    parser.add_argument('-o', '--output', required=True, help="directory for output files.")
-    parser.add_argument('-p', '--processes', help="max spawnable processes used by multiprocessing", default=-1)
+    parser.add_argument('-d', '--directory', required=True, help="directory to ps2 collection, also the output directory")
+    parser.add_argument('-p', '--processes', type=int, help="max spawnable processes used by multiprocessing", default=-1)
 
     args = parser.parse_args()
     return args
@@ -22,7 +21,7 @@ def main():
 
     # converting all images in a dir from bin to png
     print("Converting bins to pngs")
-    bin_conversion.convert_dirs(args.directory, processes=-1)
+    bin_conversion.convert_dirs(args.directory, processes=args.processes)
 
 if __name__ == "__main__":
     main()
