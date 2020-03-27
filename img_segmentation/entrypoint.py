7726149c61c48968ee3ba5bd4a8a2e0898da0599
John Xu <zhxu73@email.arizona.edu>
2020-03-27 12:22:02 -0700

    remove -o in entrypoint for img_segmentation and make use of -p

--- img_segmentation/entrypoint.py
+++ img_segmentation/entrypoint.py
@@ -10,9 +10,8 @@ import argparse
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-d', '--directory', required=True, help="directory to ps2 collection")
-    parser.add_argument('-o', '--output', required=True, help="directory for output files.")
-    parser.add_argument('-p', '--processes', help="max spawnable processes used by multiprocessing", default=-1)
+    parser.add_argument('-d', '--directory', required=True, help="directory to ps2 collection, also the output directory")
+    parser.add_argument('-p', '--processes', type=int, help="max spawnable processes used by multiprocessing", default=-1)
 
     args = parser.parse_args()
     return args
@@ -22,7 +21,7 @@ def main():
 
     # segmenting images using new python macro
     print("segmenting images")
-    image_segmentation.process_collection(args.directory)
+    image_segmentation.process_collection(args.directory, args.processes)
 
 if __name__ == "__main__":
     main()
